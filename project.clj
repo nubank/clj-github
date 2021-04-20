28c0112cd9bdd9565aa59a62e5be89b400bb646f
Phillip Mates <phillip.mates@nubank.com.br>
2021-04-20 14:40:02 +0200

    set version for release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-github "0.1.0-SNAPSHOT"
+(defproject dev.nubank/clj-github "0.1.0"
   :description "A Clojure library for interacting with the github developer API"
   :url "https://github.com/nubank/clj-github"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -24,13 +24,14 @@
   :cljfmt {:indents {flow       [[:block 1]]
                      assoc-some [[:block 0]]}}
 
-  :profiles {:dev {:dependencies   [[ch.qos.logback/logback-classic "1.3.0-alpha4" :exclusions [com.sun.mail/javax.mail]]
-                                    [org.clojure/test.check "1.1.0"]
-                                    [nubank/matcher-combinators "3.1.4" :exclusions [mvxcvi/puget commons-codec]]
-                                    [tortue/spy "2.0.0"]
-                                    [http-kit.fake "0.2.2"]
-                                    [metosin/reitit-core "0.5.10"]
-                                    [dev.nubank/clj-github-mock "0.1.0"]]}}
+  :profiles {:dev {:plugins [[lein-project-version "0.1.0"]]
+                   :dependencies [[ch.qos.logback/logback-classic "1.3.0-alpha4" :exclusions [com.sun.mail/javax.mail]]
+                                  [org.clojure/test.check "1.1.0"]
+                                  [nubank/matcher-combinators "3.1.4" :exclusions [mvxcvi/puget commons-codec]]
+                                  [tortue/spy "2.0.0"]
+                                  [http-kit.fake "0.2.2"]
+                                  [metosin/reitit-core "0.5.10"]
+                                  [dev.nubank/clj-github-mock "0.1.0"]]}}
 
   :aliases {"coverage" ["cloverage" "-s" "coverage"]
             "lint"     ["do" ["cljfmt" "check"] ["nsorg"]]
